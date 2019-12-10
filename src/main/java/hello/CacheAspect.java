33c89f74e3c79f09bf86ee64428ba932484be972
huangjiaxi <huangjiaxi@oak-inc.com>
2019-12-10 10:48:51 +0800

    基于 Map 的 AOP缓存

--- src/main/java/hello/CacheAspect.java
+++ src/main/java/hello/CacheAspect.java
@@ -3,16 +3,36 @@ package hello;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // @Aspect 声明它是一个切面
 // @Configuration 是跟 spring有关的配置 运行的时候把它拷贝进来
 @Aspect
 @Configuration
 public class CacheAspect {
+    Map<String,Object> cache = new HashMap<>();
+
     @Around("@annotation(hello.anno.Cache)")
     public Object cache(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("method is called");
-        return joinPoint.proceed();
+
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        String methodName = signature.getName();
+        Object cacheValue = cache.get(methodName);
+
+        if(cacheValue != null){
+            System.out.println("cacheValue");
+            return cacheValue;
+        }else{
+            System.out.println("read realValue");
+            Object realValue = joinPoint.proceed();
+            cache.put(methodName,realValue);
+            return realValue;
+        }
+
     }
 }
